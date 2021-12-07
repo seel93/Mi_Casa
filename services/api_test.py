5c5a26d0a594131d409ae6ea38fcd3049a4af0cc
Øystein Seel <A2110211@nbsemp.no>
2021-12-07 16:02:21 +0100

    converting json-result to dataclasses

--- services/api_test.py
+++ services/api_test.py
@@ -1,5 +1,6 @@
 from services import weather_service
 from services import yr_constants
+from dao import weather_summary
 import requests
 
 
@@ -9,7 +10,17 @@ def yr_test_api_health():
 
 
 def yr_test_location(payload_lat_lon):
-    weather_service.yr_location(payload_lat_lon)
+    location = weather_service.yr_location(payload_lat_lon)
+    location = location.get('properties').get('timeseries')
+    summary = weather_summary.WeatherSummary(**location[0].get('data').get('next_12_hours').get('summary'))
+    details = weather_summary.WeatherDetails(**location[0].get('data').get('next_12_hours').get('details'))
+    response = {
+        'symbol': summary.symbol_code,
+        'confidence': summary.symbol_confidence,
+        'probability': details.probability_of_precipitation
+    }
+
+    return response
 
 
 def yr_multiple_location_test(payload_list_lat_lon):
